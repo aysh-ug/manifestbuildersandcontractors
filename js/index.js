7e2d30daf4364049cdcd8f4a50bf745e22c08201
Ochola Alfred <59102788+aysh-ug@users.noreply.github.com>
2020-08-16 02:00:55 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -42,43 +42,6 @@ function myAccFunc(id) {
   }
 }
 
-// Slideshows
-var slideIndex = 1;
-
-function plusDivs(n) {
-  slideIndex = slideIndex + n;
-  showDivs(slideIndex);
-}
-
-function showDivs(n) {
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-
-showDivs(1);
-
-// Progress Bars
-function move() {
-  var elem = document.getElementById("myBar");   
-  var width = 5;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width == 100) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-      elem.innerHTML = width * 1  + '%';
-    }
-  }
-}
-
-
 
 // SMOOTH SCROLLING
 $(document).ready(function(){
